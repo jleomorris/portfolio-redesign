f2f15a1a0564dca8df81292516c19ea7c0925262
James Morris <james-morris2010@hotmail.co.uk>
2020-08-31 19:32:21 +0100

    Add all current HTML, JS, CSS, asset files for portfolio redesign

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// const aboutImg = document.querySelectorAll("#about-img");
+// const aboutImgHeight = aboutImg[0].clientHeight;
+// const aboutImgWidth = aboutImg[0].clientWidth;
+
+// console.info("aboutImg :", aboutImgHeight + " x " + aboutImgWidth );
+
+// const aboutDiv = document.querySelectorAll(".about-img");
+
+// let aboutDivHeight = aboutDiv[0].clientHeight;
+// let aboutDivWidth = aboutDiv[0].clientWidth;
+
+// aboutDiv[0].clientHeight = aboutImgHeight;
+// aboutDiv[0].clientWidth = aboutImgWidth;
+
+// // console.info("aboutDiv :", aboutDivHeight + " x " + aboutDivWidth );
+
+// // debugger;
---
+++ js/liquidAnimation.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.1,
+  image1: './img/about.jpg',
+  image2: './img/kolink9.png',
+  angle: Math.PI / 4, // Default
+  displacementImage: './img/stripe1.png',
+  speedIn: 4,
+  speedOut: 4,
+});
